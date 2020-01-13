7b88741b88e15660a7d25934b3ef100bc0b6575a
faraaztqureshi <faraaztqureshi@users.noreply.github.com>
2020-01-13 13:29:08 -0800

    added utility functions

--- lambdata_faraaztqureshi/__init__.py
+++ lambdata_faraaztqureshi/__init__.py
@@ -1,12 +1,10 @@
-''' lambdata - a collection of data science helper functions '''
+"""
+lambdata - a collection of data science helper functions
+"""
 
 import pandas as pd
 import numpy as np
 
-# example datasets
+#sample code
 ONES = pd.DataFrame(np.ones(10))
-ZEROES = pd.DataFrame(np.zeros(50))
-
-# example functions
-def increment(x):
-    return (x + 1)
+ZEROS = pd.DataFrame(np.zeros(50))
