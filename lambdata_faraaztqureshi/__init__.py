8cb9b99528eed7f71e71e3ab88557356f0dce834
faraaztqureshi <faraaztqureshi@users.noreply.github.com>
2020-01-14 08:58:17 -0800

    moved some files

--- lambdata_faraaztqureshi/__init__.py
+++ lambdata_faraaztqureshi/__init__.py
@@ -8,3 +8,16 @@ import numpy as np
 #sample code
 ONES = pd.DataFrame(np.ones(10))
 ZEROS = pd.DataFrame(np.zeros(50))
+
+def get_feature_names(df):
+     '''
+     Pass in Dataframe to retrieve a list
+     of columns 
+     '''
+     return df.columns
+
+def null_summary(df):
+    '''
+    Pass in Dataframe to get list of nulls
+    '''
+    return df.isnull().sum()
