8403d13c8ebe5c34a22b15cd5daeecd950a85440
faraaztqureshi <faraaztqureshi@users.noreply.github.com>
2020-01-14 11:44:37 -0800

    added a class

--- lambdata_faraaztqureshi/__init__.py
+++ lambdata_faraaztqureshi/__init__.py
@@ -9,15 +9,35 @@ import numpy as np
 ONES = pd.DataFrame(np.ones(10))
 ZEROS = pd.DataFrame(np.zeros(50))
 
-def get_feature_names(df):
-     '''
-     Pass in Dataframe to retrieve a list
-     of columns 
-     '''
-     return df.columns
+"""
+Class to help clean data
+"""
 
-def null_summary(df):
+import numpy
+import pandas
+
+class Data_cleaning(object):
+    '''This class will contains methods that can be used
+    in order to clean and analyze data
     '''
-    Pass in Dataframe to get list of nulls
-    '''
-    return df.isnull().sum()
+
+    def __init__(self):
+        pass
+        
+    def shape(df):
+        ''' Pass in DataFrame and get result of rows and columns
+        '''
+        return df.shape()
+
+    def get_feature_names(df):
+        '''
+        Pass in Dataframe to retrieve a list
+        of columns 
+        '''
+        return df.columns
+
+    def null_summary(df):
+        '''
+        Pass in Dataframe to get list of nulls
+        '''
+        return df.isnull().sum()
--- lambdata_faraaztqureshi/df_utils.py
+++ lambdata_faraaztqureshi/df_utils.py
@@ -5,16 +5,3 @@ utility functions for working with DataFrames
 import pandas
 import numpy
 TEST_DF = pandas.DataFrame([1,2,3])
-
-def get_feature_names(df):
-     '''
-     Pass in Dataframe to retrieve a list
-     of columns 
-     '''
-     return df.columns
-
-def null_summary(df):
-    '''
-    Pass in Dataframe to get list of nulls
-    '''
-    return df.isnull().sum()
