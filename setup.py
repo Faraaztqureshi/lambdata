5218517aec4dc2390942d5f83e390f3629d8048f
faraaztqureshi <faraaztqureshi@users.noreply.github.com>
2020-01-14 08:51:52 -0800

    Deleted some files to remake

--- setup.py
+++ setup.py
@@ -12,12 +12,12 @@ with open("README.md", "r") as fh:
     LONG_DESCRIPTION = fh.read()
     setuptools.setup(
     name="lambdata-faraaztqureshi",
-    version = "0.1.2",
+    version = "0.2.2",
     author = "faraaztqureshi",
     description = "a collection of data science helper functions",
     long_description = LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
-    url="https://lambdaschool.com/courses/data-science",
+    url="https://github.com/Faraaztqureshi/lambdata",
     packages=setuptools.find_packages(),
     python_requires=">=3.5",
     install_requires = REQUIRED,
